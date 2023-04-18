670fce1b7b6c6774a379e98b1bb150ceb48e1862
triqutran <triqutran@gmail.com>
2023-04-18 16:32:21 +0700

    Recover the initial base

--- android/app/src/main/java/com/nihaoma/MainActivity.java
+++ android/app/src/main/java/com/nihaoma/MainActivity.java
@@ -3,10 +3,7 @@ package com.nihaoma;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.speech.tts.TextToSpeech;
-
 import androidx.annotation.Nullable;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -15,15 +12,6 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import java.util.Objects;
 
 public class MainActivity extends ReactActivity {
-
-  private static TextToSpeech mTts;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    mTts = new TextToSpeech(this, null);
-  }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -41,34 +29,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
-        this,
+            this,
             Objects.requireNonNull(getMainComponentName()),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        );
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-      // success, create the TTS instance
-      mTts = new TextToSpeech(this, null);
-    } else {
-      // missing data, install it
-      Intent installIntent = new Intent();
-      installIntent.setAction(
-              TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-      startActivity(installIntent);
-    }
-    if (mTts == null) {
-      Intent installIntent = new Intent();
-      installIntent.setAction(
-              TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-      startActivity(installIntent);
-    }
-    mTts = new TextToSpeech(this, null);
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+            // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+            DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+    );
   }
 }
