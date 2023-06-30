a4e640bfa6c054b53f94c899bc386a435e4b1052
triqutran <triqutran@gmail.com>
2023-06-30 17:22:10 +0700

    Upgrade react-native following latest version

--- android/app/src/main/java/com/nihaoma/MainActivity.java
+++ android/app/src/main/java/com/nihaoma/MainActivity.java
@@ -1,17 +1,12 @@
 package com.nihaoma;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import androidx.annotation.Nullable;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-import java.util.Objects;
-
 public class MainActivity extends ReactActivity {
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -29,12 +24,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
-            this,
-            Objects.requireNonNull(getMainComponentName()),
-            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-            DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-            // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
-            DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-    );
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 }
